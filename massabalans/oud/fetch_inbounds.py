6e0326041c72351ab3e658fb79d05f44e789d32c
mgsrood <mgsrood@gmail.com>
2024-06-17 12:44:29 +0200

    Update 2024-06-17 12:44:28

--- massabalans/oud/fetch_inbounds.py
+++ massabalans/oud/fetch_inbounds.py
@@ -1,24 +1,18 @@
 import requests
 from requests.auth import HTTPBasicAuth
-import json
-from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
-from typing import List, Optional
 
 # Import keys.env
 load_dotenv()
 
 # Define variables
-api_url: str = os.getenv("MONTA_API_URL", "")
-username: str = os.getenv("MONTA_USERNAME", "")
-password: str = os.getenv("MONTA_PASSWORD", "")
-
-if api_url is None or username is None or password is None:
-    raise ValueError("API URL, username, or password environment variables are not set.")
+api_url = os.getenv("MONTA_API_URL", "")
+username = os.getenv("MONTA_USERNAME", "")
+password = os.getenv("MONTA_PASSWORD", "")
 
 def retrieve_inboud():
-    endpoint = 'inbounds'
+    endpoint = 'inbounds?sinceid=1'
     full_url = api_url + endpoint
     print(full_url)
     response = requests.get(full_url, auth=HTTPBasicAuth(username, password))
